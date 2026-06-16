7b28b27613c1f0b03b6c9505ba609896e3ac6f2e
bmheades <bmheades@gmail.com>
2026-06-15 23:29:35 -0700

    feat: squish buttons

--- src/lib/Indicator.svelte
+++ src/lib/Indicator.svelte
@@ -2,8 +2,8 @@
     let {id, isSlop} = $props()
 </script>
 
-<div class="">
-  <div>
-    <p class="" >{id}</p>
-  </div>
+<!-- <div class="inline-block text-[1.25rem] rounded-sm bg-zinc-600/30 px-2 py-0.5 font-semibold text-orange-400"> -->
+<div class="inline-block p-2 text-base bg-orange-300">
+
+    <p class="">{id}</p>
 </div>
--- src/lib/Report.svelte
+++ src/lib/Report.svelte
@@ -6,31 +6,39 @@
     let { id } = $props();
 
     let vote: null | boolean = $state(null);
-    storage.getItem<boolean>(`sync:${id}`).then((liked) => (vote = liked));
+    // storage.getItem<boolean>(`sync:${id}`).then((liked) => (vote = liked));
 
-    $effect(() => {
-        storage.setItem<boolean>(`sync:${id}`, vote);
-    });
+    // $effect(() => {
+    //     storage.setItem<boolean>(`sync:${id}`, vote);
+    // });
+
+    console.log(vote)
 
     function upVote() {
         vote = true;
+        console.log('voted Not slop')
     }
 
     function downVote() {
         vote = false;
+        console.log('voted slop')
     }
 </script>
 
 
-<div class="bg-amber-400 flex justify-end">
-    <button
+<div class="pt-1 font-semibold ">
+    <button disabled={vote === true}
+        class="bg-lime-400 border-lime-600 {vote === true? "translate-y-2.5 border-b": ""}"
         onclick={() => {
-            alert(id + " is Not slop");
+            // alert(id + " is Not slop");
+            upVote()
         }}
-        class="bg-amber-300">Not Slop</button>
-    <button
+        >Not Slop</button>
+    <button disabled={vote === false && vote !== null}
+        class="bg-orange-300 border-orange-500 {vote === false && vote!== null? "translate-y-2.5 border-b": ""}"
         onclick={() => {
-            alert(id + " is Slop");
+            // alert(id + " is Slop");
+            downVote()
         }}
-        class="">Report Slop</button>
+        >Slop</button>
 </div>
